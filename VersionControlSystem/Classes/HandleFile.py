4249af8b72fc774708d4ca606f3c23d35d6a76e8
Krupesh <krups224@gmail.com>
2024-01-27 01:45:37 +0530

    commit rmcommit command completed

--- VersionControlSystem/Classes/HandleFile.py
+++ VersionControlSystem/Classes/HandleFile.py
@@ -1,3 +1,4 @@
+import base64
 import hashlib
 import os
 import json
@@ -30,12 +31,30 @@ class HandleFile:
         file = open(users_file, 'a')
         file.write(f"{timestamp} {username}\n")
 
-    def get_tracked_files(self, index_file):
-        file = open(index_file, 'r')
+    def get_tracked_files(self, add_file):
+        file = open(add_file, 'r')
         files_data = json.load(file)
         return list(files_data.keys())
 
-    def compute_MD5(self, file_path):
+    def get_untracked_files(self, added_file):
+        tracked_files_data = self.read_JSON_file(added_file)
+        untracked_files = []
+
+        for root, dirs, files in os.walk(os.getcwd()):
+            dirs[:] = [d for d in dirs if d not in [
+                '.krups', 'Classes', '__pycache__', '.git']]
+            # print("dirs: ", dirs)
+            for file in files:
+                file_path = os.path.join(root, file)
+                hash = self.compute_MD5_file(file_path)
+                rel_path = os.path.relpath(file_path, os.getcwd())
+
+                if not (rel_path in tracked_files_data.keys() and tracked_files_data[rel_path] == hash):
+                    untracked_files.append(rel_path)
+
+        return untracked_files
+
+    def compute_MD5_file(self, file_path):
         BUF_SIZE = 65536
         md5 = hashlib.md5()
 
@@ -48,20 +67,37 @@ class HandleFile:
 
         return md5.hexdigest()
 
+    def compute_MD5_str(self, string):
+        return hashlib.md5(json.dumps(string).encode()).hexdigest()
+
+    def encode_base64_file(self, file_path):
+        file = open(file_path, 'rb')
+        data = file.read()
+        return base64.b64encode(data).decode('utf-8')
+
+    def decode_base64_file(self, encoded_data):
+        return base64.b64decode(encoded_data)
+
     def is_change_to_commit(self, added_file):
         file = open(added_file, 'r')
         added_files = json.load(file)
 
         return bool(added_files)
 
-    def get_latest_commit_filenames(self, commit_file):
-        file = open(commit_file, 'r')
-        commit_data = json.load(file)
+    def get_last_commit(self, head_file_path):
+        head_content = open(head_file_path).read()
+        head_lines = head_content.split('\n')
 
-        sorted_commit_data = sorted(
-            commit_data.values(), key=lambda x: json.loads(x)['timestamp'], reverse=True)
+        last_line = None
+        for line in reversed(head_lines):
+            if line.strip():
+                last_line = line.strip()
+                break
 
-        latest_commit_filenames = [entry['filename']
-                                   for entry in sorted_commit_data]
+        return last_line
 
-        return latest_commit_filenames
+    def remove_last_line(self, HEAD_path):
+        lines = open(HEAD_path, 'r').readlines()
+        if lines:
+            lines = lines[:-1]
+        open(HEAD_path, 'w').writelines(lines)
--- VersionControlSystem/VCS.py
+++ VersionControlSystem/VCS.py
@@ -1,8 +1,6 @@
 import hashlib
 import os
 import sys
-import shutil
-import json
 from datetime import datetime
 from Classes.HandleFile import HandleFile
 from Classes.Stack import Stack
@@ -11,18 +9,27 @@ from Classes.Stack import Stack
 class VersionControlSystem:
     def __init__(self, vcs_name=".tico"):
         self.vcs_name = vcs_name
-        self.username = None
+        self.branch = "main"
         self.branches_dir = os.path.join(vcs_name, "branches")
         self.objects_dir = os.path.join(vcs_name, "objects")
-        self.main_dir = os.path.join(self.branches_dir, "main")
+        self.main_branch = os.path.join(self.branches_dir, "main")
         self.added_file = os.path.join(vcs_name, "added.json")
         self.index_file = os.path.join(vcs_name, "index.json")
         self.users_file = os.path.join(vcs_name, "users.txt")
-        self.commit_file = os.path.join(self.objects_dir, "commit.json")
+        self.commits_dir = os.path.join(self.objects_dir, "commits")
+        self.content_dir = os.path.join(self.objects_dir, "content")
 
-        # initialize the file handler
+        # initialize helper classes
         self.file_handler = HandleFile()
         self.stack = Stack()
+
+    def create_branch(self, branch_name):
+        branch_path = os.path.join(self.branches_dir, branch_name)
+        if not os.path.exists(branch_path):
+            os.makedirs(branch_path)
+            print(f"Branch {branch_name} created successfully.")
+
+        self.file_handler.create_file(os.path.join(branch_path, "HEAD"))
 
     def notInitialized(self, dir_path):
         files_and_dirs = os.listdir(dir_path)
@@ -32,24 +39,24 @@ class VersionControlSystem:
 
     def init(self):
         username = input("Enter your username: ")
-        self.username = username
 
         # Create all directories
         os.makedirs(self.vcs_name, exist_ok=True)
         os.makedirs(self.branches_dir, exist_ok=True)
         os.makedirs(self.objects_dir, exist_ok=True)
-        os.makedirs(self.main_dir, exist_ok=True)
+        os.makedirs(self.content_dir, exist_ok=True)
+        os.makedirs(self.commits_dir, exist_ok=True)
+        self.create_branch("main")
 
         # Create files
         self.file_handler.create_file(self.added_file)
         self.file_handler.create_file(self.index_file)
         self.file_handler.create_file(self.users_file)
-        self.file_handler.create_file(self.commit_file)
 
         # Append user details
         self.file_handler.append_user_details(self.users_file, username)
 
-        print("New Empty .tico repository created.")
+        print("New Empty .krups repository created.")
 
     def status(self):
         if self.notInitialized('.'):
@@ -59,9 +66,32 @@ class VersionControlSystem:
 
         tracked_files_data = self.file_handler.read_JSON_file(self.added_file)
 
-        def is_subdirectory(parent, child):
-            relative_path = os.path.relpath(child, parent)
-            return not relative_path.startswith('..') and not os.path.isabs(relative_path)
+        if not tracked_files_data:
+            HEAD_path = os.path.join(self.branches_dir, self.branch, 'HEAD')
+            last_commit = self.file_handler.get_last_commit(HEAD_path)
+
+            if last_commit:
+                commit_file = self.file_handler.read_JSON_file(
+                    os.path.join(self.commits_dir, f"{last_commit}.json"))
+                committed_files = commit_file['change']
+                all_commited = True
+
+                for root, dirs, files in os.walk(os.getcwd()):
+                    dirs[:] = [d for d in dirs if d not in [
+                        '.krups', 'Classes', '__pycache__', '.git']]
+                    # print("dirs: ", dirs)
+                    for file in files:
+                        file_path = os.path.join(root, file)
+                        hash = self.file_handler.compute_MD5_file(file_path)
+                        rel_path = os.path.relpath(file_path, os.getcwd())
+
+                        if not (rel_path in committed_files.keys() and committed_files[rel_path] == hash):
+                            all_commited = False
+                            tracked_files_data = committed_files
+
+                if all_commited:
+                    print("All files committed...")
+                    return
 
         for root, dirs, files in os.walk(os.getcwd()):
             dirs[:] = [d for d in dirs if d not in [
@@ -69,7 +99,7 @@ class VersionControlSystem:
             # print("dirs: ", dirs)
             for file in files:
                 file_path = os.path.join(root, file)
-                hash = self.file_handler.compute_MD5(file_path)
+                hash = self.file_handler.compute_MD5_file(file_path)
                 rel_path = os.path.relpath(file_path, os.getcwd())
 
                 if rel_path in tracked_files_data.keys() and tracked_files_data[rel_path] == hash:
@@ -84,13 +114,13 @@ class VersionControlSystem:
             print(f"Error: File '{file_path_relative}' does not exist.")
             return
 
-        file_hash_MD5 = self.file_handler.compute_MD5(file_path_full)
+        file_path_hash = self.file_handler.compute_MD5_file(file_path_full)
         file_path_relative = file_path_relative if file_path_relative else os.path.normpath(
             file_path_full)
         self.file_handler.add_JSON_data(
-            self.added_file, file_path_relative, file_hash_MD5)
+            self.added_file, file_path_relative, file_path_hash)
         self.file_handler.add_JSON_data(
-            self.index_file, file_path_relative, file_hash_MD5)
+            self.index_file, file_path_relative, file_path_hash)
 
     def add_with_subdirs(self, dir_path):
         if self.notInitialized('.'):
@@ -112,11 +142,7 @@ class VersionControlSystem:
                     file_path_full)
                 self.add(file_path_full, file_path_relative)
 
-    def commit(self):
-        if len(sys.argv) < 4 or sys.argv[2] != '-m':
-            print("Usage: python VCS.py commit -m <message>")
-            return
-
+    def commit(self, message="New commit"):
         if self.notInitialized('.'):
             print("'.krups' folder is not initialized...")
             print("Run: 'tico init' command to initialize tico repository")
@@ -126,25 +152,51 @@ class VersionControlSystem:
             print("No changes to commit.")
             return
 
-        message = sys.argv[3]
-        added_files = self.file_handler.get_tracked_files(self.added_file)
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        tracked_files = self.file_handler.get_tracked_files(self.added_file)
 
-        JSON_data = self.file_handler.read_JSON_file(self.commit_file)
+        untracked_files = self.file_handler.get_untracked_files(
+            self.added_file)
+        if untracked_files:
+            print("\nUntracked files present.")
+            for file in untracked_files:
+                print("Untracked: ", file)
+            print()
+            ans = input("Do you want to commit untracked files? (y/n): ")
+            if ans == 'y':
+                self.add_with_subdirs('.')
+        elif not tracked_files:
+            print("No changes to commit")
+            return
 
-        for filename in added_files:
-            commit_data = {
-                'filename': filename,
-                'message': message,
-                'author': self.username,
-                'timestamp': timestamp
-            }
+        timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        changes = {}
+        for file_path in tracked_files:
+            file_path_full = os.path.normpath(
+                os.path.join(os.getcwd(), file_path))
+            changes[file_path] = self.file_handler.compute_MD5_file(
+                file_path_full)
 
-            file_hash = hashlib.md5(json.dumps(
-                commit_data, sort_keys=True).encode()).hexdigest()
-            JSON_data[filename] = file_hash
+        commit_data = {
+            "message": message,
+            "timestamp": timestamp,
+            "change": changes,
+            "branch": self.branch
+        }
 
-        self.file_handler.write_JSON_file(self.commit_file, JSON_data)
+        commit_data_hash = self.file_handler.compute_MD5_str(commit_data)
+        head_file = open(os.path.join(
+            self.branches_dir, self.branch, 'HEAD'), 'a')
+        head_file.write(commit_data_hash + '\n')
+
+        self.file_handler.write_JSON_file(os.path.join(
+            self.commits_dir, f"{commit_data_hash}.json"), commit_data)
+
+        for file_path, file_hash in changes.items():
+            file_data_encrypted = self.file_handler.encode_base64_file(os.path.normpath(
+                os.path.join(os.getcwd(), file_path)))
+            open(os.path.join(self.content_dir,
+                              file_hash + '.txt'), 'w').write(file_data_encrypted)
+
         self.file_handler.write_JSON_file(self.added_file, {})
 
     def rmcommit(self):
@@ -153,15 +205,34 @@ class VersionControlSystem:
             print("Run: 'tico init' command to initialize tico repository")
             return
 
-        latest_commit_filenames = self.file_handler.get_latest_commit_filenames(
-            self.commit_file)
+        HEAD_path = os.path.join(self.branches_dir, self.branch, 'HEAD')
+        last_commit = self.file_handler.get_last_commit(HEAD_path)
 
-        if not latest_commit_filenames:
-            print("No commits to remove")
+        if not last_commit:
+            print("No commits done...")
             return
 
-        for filename in latest_commit_filenames:
-            self.add(filename)
+        commit_file = self.file_handler.read_JSON_file(
+            os.path.join(self.commits_dir, f"{last_commit}.json"))
+        committed_files = commit_file['change']
+
+        for file_path, file_hash in committed_files.items():
+            file_path_encoded_data = os.path.join(
+                self.content_dir, file_hash+'.txt')
+            file_path_decoded_data = os.path.normpath(
+                os.path.join(os.getcwd(), file_path))
+
+            file_data_encoded = open(file_path_encoded_data, 'r').read()
+            file_data_decoded = self.file_handler.decode_base64_file(
+                file_data_encoded)
+            open(file_path_decoded_data, 'wb').write(file_data_decoded)
+
+            os.remove(file_path_encoded_data)
+
+        commit_file_path = os.path.join(
+            self.commits_dir, f"{last_commit}.json")
+        os.remove(commit_file_path)
+        self.file_handler.remove_last_line(HEAD_path)
 
     def help(self):
         print("Tico - A Version Control System.")
@@ -182,19 +253,64 @@ class VersionControlSystem:
 
 vcs = VersionControlSystem('.krups')
 
+
 if len(sys.argv) < 2:
     vcs.help()
     sys.exit(1)
 
 command = sys.argv[1]
 
-commands = {
-    "init": vcs.init,
-    "status": vcs.status,
-    "add": lambda: vcs.add_with_subdirs(sys.argv[2]) if len(sys.argv) > 2 else print("File argument missing"),
-    "help": vcs.help,
-    "rmcommit": vcs.rmcommit,
-    "commit": vcs.commit,
-}
+if command == "init":
+    if os.path.exists('.krups'):
+        print(f"'.krups' alerady initialized...")
+        sys.exit()
 
-commands.get(command, lambda: print("Not a valid command"))()
+    vcs.init()
+    sys.exit()
+
+elif command == "status":
+    if len(sys.argv) < 2 or len(sys.argv) > 2:
+        print("Usage: krups status")
+        sys.exit()
+
+    vcs.status()
+
+
+elif command == "add":
+    if len(sys.argv) < 3:
+        print("Usage: krups add <files>")
+        sys.exit()
+
+    for arg in sys.argv[2:]:
+        vcs.add_with_subdirs(arg)
+    sys.exit()
+
+elif command == "commit":
+    if len(sys.argv) == 3 and sys.argv[2] == '-m':
+        vcs.commit()
+    elif len(sys.argv) == 4 and sys.argv[2] == '-m':
+        vcs.commit(sys.argv[3])
+    else:
+        print("Usage: krups commit -m <message>")
+
+    sys.exit()
+
+elif command == "rmcommit":
+    if len(sys.argv) < 2 or len(sys.argv) > 2:
+        vcs.help()
+        sys.exit()
+
+    vcs.rmcommit()
+
+elif command == 'help':
+    if len(sys.argv) < 2 or len(sys.argv) > 2:
+        print("Usage: krups help")
+        sys.exit()
+
+    vcs.help()
+    print(vcs.username)
+    sys.exit()
+
+else:
+    print(f"krups: '{command}' is not a krups command. See 'krups help'")
+    sys.exit()
